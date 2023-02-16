1e09a3c929e5837b5f471603b1203bb7298bbc58
Joana Correia <redbugmilk@gmail.com>
2023-02-16 23:35:03 +0000

    call service

--- src/Posts.js
+++ src/Posts.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useEffect, useState, useCallback } from 'react'
 
 const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
+    const [pageNumber, setPageNumber] = useState(1);
+    const [data, setData] = useState();
+    const fetchPosts = useCallback(async (pageNumber) => {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
+        );
+        return response.json();
+      },[pageNumber]);
+
+    useEffect(async ()=>{
+        const data = await fetchPosts();
+        setData(data);
+    },[fetchPosts]);
+    console.log(data)
+    return (
+        <div>Posts</div>
+    )
 }
 
 export default Posts
